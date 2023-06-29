9975d4d0d724a7eeefec1a8c9cbd77702bb0b555
Martin Rosas <mlrosas21@hotmail.com>
2023-06-29 00:00:35 -0300

    + mock quotes

---
+++ src/mocks/quotes.ts
@@ -0,0 +1,36 @@
+export const randomQuote = [
+  {
+    quote:
+      "Shoplifting is a victimless crime, like punching someone in the dark.",
+    character: "Nelson Muntz",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
+    characterDirection: "Left",
+  },
+];
+
+export const mockQuotes = [
+  {
+    quote:
+      "All I'm gonna use this bed for is sleeping, eating and maybe building a little fort.",
+    character: "Homer Simpson",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
+    characterDirection: "Right",
+  },
+  {
+    quote: "Ahh! Sweet liquor eases the pain.",
+    character: "Troy McClure",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FTroyMcClure.png?1497567511112",
+    characterDirection: "Right",
+  },
+  {
+    quote:
+      "I live in a single room above a bowling alley...and below another bowling alley.",
+    character: "Frank Grimes",
+    image:
+      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FFrankGrimes.png?1497567511887",
+    characterDirection: "Left",
+  },
+];
